61292fa6d32848b11aba4f92fd11c23d5640bb34
nguyenvu1997 <nguyenanhvu19971997@gmail.com>
2021-07-01 22:14:58 +0700

    Commit v1

--- test_deploy/settings.py
+++ test_deploy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-san&)nr&5#esz+%f9xt^^-mc6h6h8jc$_u#h6#(9qlzaa#syg-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
